5bab18a541fe88566e4ca8122c459c8465d6441f
martsa <Thinktibet123>
2021-01-31 22:36:15 +0100

    initial commit- but done some major inventory works

---
+++ BasicInventoryApp/src/main/java/com/meshupProjekt/Constants.java
@@ -0,0 +1,5 @@
+package com.meshupProjekt;
+
+public class Constants {
+	public static final int PAGE_SIZE = 5;
+}
---
+++ BasicInventoryApp/src/test/java/com/meshupprojekttest/InventoryAppApplicationTests.java
@@ -0,0 +1,18 @@
+package com.meshupprojekttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+class InventoryAppApplicationTests {
+
+	@Test
+	public void test() {
+	}
+	
+}
